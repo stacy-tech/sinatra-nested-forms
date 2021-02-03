c49674a1458b0a6c2c327676833aeebd1b74a863
stacy <stacydn23@gmail.com>
2021-02-02 21:15:40 -0500

    commit tree c56aa7055efdade8d5e87d440b87ada944c16096

--- app.rb
+++ app.rb
@@ -5,5 +5,25 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do 
+      erb :root
+    end
+
+    get '/new' do 
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new params[:pirate]
+
+      params[:pirate][:ships].each do |ship|
+        Ship.new ship
+      end
+      @ship1 = Ship.all.first
+      @ship2 = Ship.all[1]
+
+      erb :'pirates/show'
+    end
+
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+
+    attr_accessor :name, :type, :booty
+    @@all = []
+
+    def initialize(args)
+        @name = args[:name]
+        @topic = args[:topic]
+        @booty = args[:booty]
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
 end
